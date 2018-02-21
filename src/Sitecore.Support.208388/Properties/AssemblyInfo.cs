54dad0ebfb2b6e9fa2044ee823c6d0f819c03429
Kirill Volovitskiy <kiv@sitecore.net>
2018-02-21 13:41:18 +0200

    initial commit

---
+++ src/Sitecore.Support.208388/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.208388")]
+[assembly: AssemblyProduct("Sitecore.Support.208388")]
+[assembly: ComVisible(false)]
